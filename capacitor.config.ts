8f193adbb15059d27193e4bcdbc86b57b13467b6
lvfe frh <ilya.gruzinskiy@bk.ru>
2025-01-06 13:35:57 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dziennik.pod',
+  appName: 'dziennik-pod',
+  webDir: 'dist'
+};
+
+export default config;
