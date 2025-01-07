e85abf709dff7345109ab8893b3bed096fb4c84a
lvfe frh <ilya.gruzinskiy@bk.ru>
2025-01-07 15:00:44 +0300

    Adding the ability to save photos to the file system

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -2,32 +2,85 @@ import { ref, onMounted, watch } from 'vue';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
-import { WebView } from '@capacitor/core';
 
 export const usePhotoGallery = () => {
-    const photos = ref<UserPhoto[]>([]);
-    const takePhoto = async () => {
-      const photo = await Camera.getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100,
-      });
-      const fileName = Date.now() + '.jpeg';
-      const savedFileImage = {
-        filepath: fileName,    
-        webviewPath: photo.webPath,
-      };
+  const photos = ref<UserPhoto[]>([]);
 
-      photos.value = [savedFileImage, ...photos.value];
-    };
-  
+  const takePhoto = async () => {
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+    const fileName = Date.now() + '.jpeg';
+    const savedFileImage = await savePicture(photo, fileName);
+    photos.value = [savedFileImage, ...photos.value];
+  };
+
+  const PHOTO_STORAGE = 'photos';
+
+  const convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    const base64Data = (await convertBlobToBase64(blob)) as string;
+
+    await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
     return {
-        photos,
-        takePhoto,
+      filepath: fileName,
+      webviewPath: photo.webPath,
     };
   };
 
-  export interface UserPhoto{
-    filepath: string;
-    webviewPath?: string;
-  }
+  const cachePhotos = () => {
+    Preferences.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(photos.value),
+    });
+  };
+
+  watch(photos, cachePhotos);
+
+  const loadSaved = async () => {
+    const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+    const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
+
+    for (const photo of photosInPreferences) {
+      const file = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+      photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+    }
+
+    photos.value = photosInPreferences;
+  };
+
+  onMounted(() => {
+    loadSaved();
+  });
+
+  return {
+    photos,
+    takePhoto,
+  };
+};
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
